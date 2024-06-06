930bf40c9bfc7a7c1351b18c72b46306d7fbd57a
Alex Shomin <alex.sh0min@yandex.ru>
2024-06-06 15:06:29 +0700

    test_commit

--- guess_number.py
+++ guess_number.py
@@ -1,17 +1,4 @@
-from random import randint
+def assdd(fir, sec):
+    return fir + sec
 
-number = randint(1, 100)
-print('Угадайте число от 1 до 100')
-
-while True:
-    guess = int(input('Введите число: '))
-    
-    if guess < number:
-        print('Ваше число меньше того, что загадано.')
-    
-    elif  guess > number:
-        print('Ваше число больше того, что загадано.')
-    
-    elif guess == number:
-        break
-print('Отличная интуиция! Вы угадали число :)')
+print('Сумма чисел: ', assdd(10, 5))
